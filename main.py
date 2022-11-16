2080cf7790b216fd15609e12ece4abfbd9f18726
paisalc <117634359+paisalc@users.noreply.github.com>
2022-11-16 14:36:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-weight = 85;
-height = 6;
-x = weight/float(height*height);
-if x < 18.5:
-print('Underweight')
-if x>=18.5 and x<25:
-print("Normal")
-if x >= 25 and x < 30:
-print('Overweight')
-if x >= 30:
-print('Obesity')
+# asking for input from the users  
+the_height = 180  
+the_weight = 85  
+# defining a function for BMI  
+the_BMI = the_weight / (the_height/100)**2  
+# printing the BMI  
+print("Your Body Mass Index is", the_BMI)  
+# using the if-elif-else conditions  
+if the_BMI <= 18.5:  
+    print("Oops! You are underweight.")  
+elif the_BMI <= 24.9:  
+    print("Awesome! You are healthy.")  
+elif the_BMI <= 29.9:  
+    print("Eee! You are over weight.")  
+else:  
+    print("Seesh! You are obese.")  
